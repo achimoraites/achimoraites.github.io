18fbc1702d9802960efaa0a5df367c51f143f787
Achilles Moraites <achimoraites@yahoo.gr>
2017-04-18 11:51:59 +0000

    Added routes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp',['ngResource','ngRoute']);
+var app = angular.module('myApp',['ngRoute']);
 
 
 app.config(['$locationProvider',
@@ -6,5 +6,20 @@ app.config(['$locationProvider',
         $locationProvider.hashPrefix('!');
     }
 ]);
+
+app.config(function($routeProvider) {
+
+  $routeProvider.when('/home',{
+    templateUrl: '/partials/home.html'
+  })
+  .when('/projects',{
+    templateUrl: '/partials/projects.html'
+  })
+  .when('/contact', {
+    templateUrl: '/partials/contact.html'
+  })
+  .otherwise({
+    redirectTo:'/home'});
+});
 
 if (window.location.hash === '#_=_') window.location.hash = '#!';
